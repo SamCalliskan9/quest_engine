fbc2e6700d689df4c4a6cf3289f1e34cf21d889f
SamCalliskan <erdincscaliskan@gmail.com>
2025-11-14 19:09:29 +0300

    Update scratch.1.py

--- scratch.1.py
+++ scratch.1.py
@@ -1,5 +1,4 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+
 >>> 
 ... def print_divider():
 ...     print("\n" * 50 + "\n")
@@ -78,6 +77,7 @@ def main():
     choose_path(name)
 
 main()
+
 
 
 
